118af773f294d18546854dc1f15667128112f8d6
Aleksey Pekhterev <qudaeo@yandex.ru>
2024-11-08 11:07:33 +0700

    feat: add sc webview

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import WebView from 'react-native-webview';
+
+function App(): React.JSX.Element {
+  return (
+    <WebView style={styles.container} source={{uri: 'https://sc.pro-cop.ru/#/'}} />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
